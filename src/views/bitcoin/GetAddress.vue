cdd4e45a3164935db0973cc61f62f475e100cec5
webees <webees@qq.com>
2020-03-02 16:38:19 +0800

    add multisig

--- src/views/bitcoin/GetAddress.vue
+++ src/views/bitcoin/GetAddress.vue
@@ -15,21 +15,14 @@
             <v-text-field v-model="d_path" label="path" hide-details />
           </v-col>
           <v-col cols="4">
-            <v-select
-              v-model="d_scriptType"
-              :items="[
-                { text: 'SPENDADDRESS', value: 0 },
-                { text: 'SPENDMULTISIG', value: 1 },
-                { text: 'EXTERNAL', value: 2 },
-                { text: 'SPENDWITNESS', value: 3 },
-                { text: 'SPENDP2SHWITNESS', value: 4 }
-              ]"
-              label="script_type"
-              hide-details
-            ></v-select>
+            <v-select v-model="d_scriptType" :items="['SPENDADDRESS', 'SPENDMULTISIG', 'EXTERNAL', 'SPENDWITNESS', 'SPENDP2SHWITNESS']" label="script_type" hide-details></v-select>
           </v-col>
         </v-row>
-        <v-row align="center"> </v-row>
+        <v-row v-if="d_scriptType === 'SPENDMULTISIG'">
+          <v-col>
+            <v-textarea label="multisig" v-model="d_multisig" auto-grow></v-textarea>
+          </v-col>
+        </v-row>
         <v-row>
           <v-col>
             <v-textarea label="Request" :value="d_request" filled readonly auto-grow></v-textarea>
@@ -56,8 +49,9 @@ export default {
   name: 'GetAddress',
   data: () => ({
     d_path: `m/49'/0'/0'/0/0`,
-    d_scriptType: 0,
+    d_scriptType: 'SPENDP2SHWITNESS',
     d_showDisplay: false,
+    d_multisig: '',
     d_response: '',
     d_request: ''
   }),
@@ -73,15 +67,36 @@ export default {
       return address_n
     }
   },
+  mounted() {
+    const multisig = {
+      pubkeys: [
+        {
+          node: 'xpub661MyMwAqRbcF1zGijBb2K6x9YiJPh58xpcCeLvTxMX6spkY3PcpJ4ABcCyWfskq5DDxM3e6Ez5ePCqG5bnPUXR4wL8TZWyoDaUdiWW7bKy',
+          address_n: [1]
+        },
+        {
+          node: 'xpub661MyMwAqRbcF1zGijBb2K6x9YiJPh58xpcCeLvTxMX6spkY3PcpJ4ABcCyWfskq5DDxM3e6Ez5ePCqG5bnPUXR4wL8TZWyoDaUdiWW7bKy',
+          address_n: [2]
+        },
+        {
+          node: 'xpub661MyMwAqRbcF1zGijBb2K6x9YiJPh58xpcCeLvTxMX6spkY3PcpJ4ABcCyWfskq5DDxM3e6Ez5ePCqG5bnPUXR4wL8TZWyoDaUdiWW7bKy',
+          address_n: [3]
+        }
+      ],
+      m: 2
+    }
+    this.d_multisig = JSON.stringify(multisig, null, 4)
+  },
   methods: {
     async getAddr() {
       if (!this.c_addressN) return (this.d_response = 'path error')
       const proto = {
         address_n: this.c_addressN,
         script_type: this.d_scriptType,
-        show_display: this.d_showDisplay
+        show_display: this.d_showDisplay,
+        multisig: this.d_scriptType === 'SPENDMULTISIG' ? JSON.parse(this.d_multisig) : undefined
       }
-      const result = await this.$usb.cmd('GetAddress', proto)
+      const result = await this.$usb.getAddr(proto)
       this.d_request = `abckey.cmd("GetAddress", ` + JSON.stringify(proto, null, 4) + ')'
       this.d_response = JSON.stringify(result, null, 4)
     }
