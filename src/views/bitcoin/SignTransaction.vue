fcce3aaf56711c7c5312ec9acd47e0f9f892c364
webees <webees@qq.com>
2020-03-18 15:02:58 +0800

    fix multisig

--- src/views/bitcoin/SignTransaction.vue
+++ src/views/bitcoin/SignTransaction.vue
@@ -30,6 +30,11 @@
         </v-row>
         <v-row>
           <v-col>
+            <v-textarea label="reftxs_json" v-model="d_utxo" auto-grow></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
             <v-textarea label="Request" :value="d_request" filled readonly auto-grow></v-textarea>
           </v-col>
         </v-row>
@@ -61,34 +66,70 @@ export default {
     d_lockTime: 0,
     d_inputs: '',
     d_outputs: '',
+    d_utxo: '',
     d_request: '',
     d_response: ''
   }),
   mounted() {
     const inputs = [
       {
-        address_n: [2147483697, 2147483650, 2147483648, 0, 0],
-        amount: '1251912615515151598',
-        prev_hash: 'f6ceb5e14b4cb7bd8a2b1922bd2d91596de3be447c806141f225c3d77a0e99eb',
+        address_n: [2147483693, 0, 0, 0],
+        prev_hash: '37826a3b7ba54faaaee0c6195973dca7b7f6221ebd1756f8285e3be49b247d21',
         prev_index: 0,
-        script_type: 'SPENDP2SHWITNESS',
-        sequence: 4294967295
+        sequence: 4294967294,
+        script_type: 'SPENDMULTISIG',
+        amount: '1000100',
+        multisig: {
+          pubkeys: [
+            {
+              node: `xpub6BbAk2NX3geR1G5eJvww9qvgXPLZZZzE8oYpi56XQUSHH8u91kGYBcQXaHAMVNUnbeQQhS87nn38X8Y5YRj4UjA7gqtLEggpqQvZBp6W3NS`,
+              address_n: [0, 0]
+            },
+            {
+              node: 'xpub6AxX9fezLTUJwu2FrRkLcnLb6hdeiBXqabWasqnfvyacrgok92VJ3H963Qb28LH56TZD2T5HkHCB9HB74ADS6c4SEjQjenb2DG4xxFo7NWU',
+              address_n: [0, 0]
+            }
+          ],
+          signatures: ['', ''],
+          m: 2
+        }
       }
     ]
     const outputs = [
       {
-        address: '1CXAKbLJW5PZiFRH5nANuDKhaPJ5TKepXD',
-        amount: '12519063',
+        address: '3L6UYMqVLxzZRJ9EpZpf3o5ZvWWs1KNjQb',
+        amount: '9',
         script_type: 'PAYTOADDRESS'
-      },
+      }
+    ]
+    const utxo = [
       {
-        address_n: [2147483697, 2147483650, 2147483648, 0, 0],
-        amount: '1251',
-        script_type: 'PAYTOADDRESS'
+        hash: '37826a3b7ba54faaaee0c6195973dca7b7f6221ebd1756f8285e3be49b247d21',
+        inputs: [
+          {
+            prev_hash: 'a653e5b4e9b1ac110540f97b97ab97cc7ad9a1e81e844b223500d68697192cd4',
+            prev_index: 0,
+            script_sig: '160014fee4152a9ec43bedacc7238b28c848c5272eb0fc',
+            sequence: 4294967294
+          }
+        ],
+        bin_outputs: [
+          {
+            amount: '1000100',
+            script_pubkey: 'a914b5619f06ad24e8a44fd8e0b14f6c095cede7aa4b87'
+          },
+          {
+            amount: '15687407',
+            script_pubkey: 'a91495183a50937681facfb6cf071548795e07c7127d87'
+          }
+        ],
+        lock_time: 621597,
+        version: 2
       }
     ]
     this.d_inputs = JSON.stringify(inputs, null, 4)
     this.d_outputs = JSON.stringify(outputs, null, 4)
+    this.d_utxo = JSON.stringify(utxo, null, 4)
   },
   methods: {
     async signTx() {
@@ -96,10 +137,10 @@ export default {
         coin_name: this.d_coinName,
         inputs: JSON.parse(this.d_inputs),
         outputs: JSON.parse(this.d_outputs),
+        utxo: JSON.parse(this.d_utxo),
         version: this.d_version,
         lock_time: this.d_lockTime
       }
-      console.log(proto)
       const result = await this.$usb.signTx(proto)
       this.d_response = JSON.stringify(result, null, 4)
     }
