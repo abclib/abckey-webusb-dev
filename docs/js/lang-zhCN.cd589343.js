5911eb05c0e49124b1af35457cadc8c631aa4e2f
webees <webees@qq.com>
2020-03-30 22:33:56 +0800

    +

---
+++ docs/js/lang-zhCN.cd589343.js
@@ -0,0 +1,9 @@
+;(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
+  ['lang-zhCN'],
+  {
+    ccc5: function (c, n, o) {
+      'use strict'
+      o.r(n), (n['default'] = { zhCN: { Home: '首页' } })
+    }
+  }
+])
