aef4acd147d42789589e029d23273605c5d15cf3
webees <webees@qq.com>
2020-03-31 12:45:41 +0800

    add link

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <v-footer class="pa-3" dark>
     <v-spacer></v-spacer>
-    <div>&copy;&nbsp;ABCKEY&nbsp;{{ new Date().getFullYear() }}</div>
+    <div>
+      <v-btn href="//abckey.com" target="_blank" text dark>
+        <span>&copy;&nbsp;ABCKEY</span>
+      </v-btn>
+      <v-btn class="mx-4" href="//github.com/abclib" target="_blank" dark icon>
+        <v-icon size="24px">mdi-github</v-icon>
+      </v-btn>
+    </div>
   </v-footer>
 </template>
 
